fbb9441eefd02de735aa0595c5e841e34dbc6696
Luis Gago <luisgago@lagc-peru.com>
2013-11-23 10:04:40 -0500

    Archivos listos para empesar
    Archivos necesarios para empesar a desarrollar la plantillas

---
+++ index.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="es_ES">
+<head>
+	<meta charset="utf-8" />
+	<title>Corasil - corasil.pe</title>
+	<link rel="shortcut icon" href="img/favicon.png" />
+	<meta name="description" content="" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="h" />
+	<meta property="og:image" content="img/icon.png" />
+	<meta property="og:site_name" content="" />
+	<meta property="fb:admins" content="100003974860941" />
+	<link rel="stylesheet" href="css/estilos.css" />
+    <script src="js/jquery-1.10.1.min.js"></script>
+    <script src="js/jquery-ui.js"></script>
+</head>
+<body>
+    <div id="cuerpo">
+        <header class="cabecera">
+            <div class="logo">
+                <img src="" alt="" />
+            </div>
+            <nav class="menu">
+                <ul>
+                    <li><a href="">Nosotros</a></li>
+                    <li><a href="">Galeria</a></li>
+                    <li><a href="">Videos</a></li>
+                    <li><a href="">Contàctenos</a></li>
+                </ul>
+            </nav>
+            <div class="redes">
+                <figure>
+                    <a href=""><img src="img/fb.png" /></a>
+                </figure>
+                <figure>
+                    <a href=""><img src="img/yb.png" /></a>
+                </figure>
+            </div>
+        </header>
+    </div>
+    <script src="js/main.js"></script>
+    <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-41465403-5', 'utpaqp.info');
+    ga('send', 'pageview');
+
+    </script>
+</body>
+</html>
